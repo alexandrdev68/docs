74c41ed635c50380e4a22a3eb33e0373a97c9de3
Alexandr <alexfree@meta.ua>
2014-04-05 14:09:55 +0300

    добавлен пример запроса к БД

--- joomla/examples.php
+++ joomla/examples.php
@@ -1,3 +1,17 @@
 <?php
+	//пример запроса к БД в joomla
+	$db = JFactory::getDbo();
+ 
+	// Create a new query object.
+	$query = $db->getQuery(true);
+	$query
+	    ->select('*')
+	    ->from($db->quoteName('u5ghb_user_profiles', 'u5ghb_user_keys'));
+		/*->where($db->quoteName('b.username') . ' LIKE \'root\'')
+	    ->order($db->quoteName('a.created') . ' DESC')*/
+	    
+	$db->setQuery($query);
 	
+	$results = $db->loadObjectList();
+	print_r($results); exit;
 ?>
