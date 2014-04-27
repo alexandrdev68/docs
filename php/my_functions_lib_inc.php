2847fb74ff25f2f6a19712913ec36efda65096da
Alexandr <alexfree@meta.ua>
2014-04-27 17:09:31 +0300

    добавлен класс для отправки запроса на сервер

--- php/my_functions_lib_inc.php
+++ php/my_functions_lib_inc.php
@@ -596,3 +596,84 @@ function parseFields($arInline, $stringXML = ''){
     return $stringXML;
   }
 ?>
+<script>
+//класс для отправки запроса на сервер посредством jQuery
+function serverRequest(params){
+	params = params || {};
+    this.type = params.type || 'POST';
+    this.events = params.events || 'on';
+    this.url = params.url || '';
+    this.query = params.query || '';
+    this.data = params.data || '';
+    if(params.traditional !== undefined) this.traditional = params.traditional;
+    else this.traditional = false;
+    if(params.queryDataFormat !== undefined) this.queryDataFormat = params.queryDataFormat;
+    else this.queryDataFormat = 'url';
+    if(params.processData !== undefined) this.processData = params.processData;
+    else this.processData = false;
+    this.need_auth = (params.need_auth !== undefined ? params.need_auth : false);
+    if(params.contentType !== undefined) this.contentType = params.contentType;
+    else this.contentType = false;
+    
+    if(params.success !== undefined) this.success = params.success;
+    else{
+    	this.success = function(response){
+	    	if(response.status == 'ok'){
+                //good response handler
+	    		
+            }else if(response.status !== undefined && response.status == 'bad'){
+                //bad response handler
+            	
+            }else{
+                //unknown response handler
+            }
+	    };
+    }
+    
+    if(params.error !== undefined) this.error = params.error;
+    else{
+    	this.error = function(response){
+	    	//ajax error handler
+	    	Pbank.showMessage('Error on server, try again later');
+	    };
+    }
+    
+    
+    this.send = function(sparams){
+        var self = this;
+        sparams = sparams || {};
+        if(sparams.type !== undefined) self.type = sparams.type;
+        if(sparams.events !== undefined) self.events = sparams.events;
+        if(sparams.url !== undefined) self.url = sparams.url;
+        if(sparams.data !== undefined) self.data = sparams.data;
+        if(sparams.query !== undefined) self.query = sparams.query;
+        if(sparams.error !== undefined) self.error = sparams.error;
+        if(sparams.success !== undefined) self.success = sparams.success;
+        if(sparams.contentType !== undefined) self.contentType = sparams.contentType;
+        if((this.queryDataFormat == 'json' || this.queryDataFormat == 'JSON') && self.data !== '') self.data = $.toJSON(self.data);
+        var ajax_params = {
+    		url: self.url + self.query,
+            type: self.type,
+            data: self.data,
+            dataType : 'json',
+            processData : true,
+            traditional : self.traditional,
+            success: function(response) {
+            	self.success(response);
+            },
+            error: function(response){
+            	if(typeof response.responseText == 'string'){
+            		self.error({error : response.responseText, response : response});
+            	}else{
+            		self.error({error : jQuery.parseJSON(response.responseText), response :response});
+            	}
+            }
+        };
+        
+        if(this.contentType !== false) ajax_params.contentType = this.contentType;
+        
+        jQuery.ajax(ajax_params);
+    };
+
+};
+</script>
