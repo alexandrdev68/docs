91710fa8d1720754ba4394c667705d19250d9982
Alexandr Borisov <sasha@ubuntuSasha>
2016-03-02 13:46:34 +0200

    Доробки в VTemplate тепер можна навішувати події на елементи автоматично

--- javascript/vtemplate.js
+++ javascript/vtemplate.js
@@ -14,6 +14,9 @@ function VTemplate(params){
 	if(!!params.functions)
 		this.functions = params.functions;
 	
+	if(!!params.eventFunctions)
+		this.eventFunctions = params.eventFunctions;
+	
 	VTemplate.prototype.addTextNode = function(element, text){
 		var textNode = document.createTextNode(text);
 		element.appendChild(textNode);
@@ -27,11 +30,68 @@ function VTemplate(params){
 		element.appendChild(document.createTextNode(text));
 	};
 	
+	if (document.addEventListener) {
+		VTemplate.prototype.addEvent = function(elem, type, handler) {
+			elem.addEventListener(type, handler, false)
+		}
+		VTemplate.prototype.removeEvent = function(elem, type, handler) {
+			elem.removeEventListener(type, handler, false)
+		}
+	}else{
+		VTemplate.prototype.addEvent = function(elem, type, handler) {
+			elem.attachEvent("on" + type, handler)
+		}
+		VTemplate.prototype.removeEvent = function(elem, type, handler) {
+			elem.detachEvent("on" + type, handler)
+		}
+	}
+	
 	this.workElement = {};
 	
 	this.afterRender = params.afterRender || function(tempElements){
 		
 	};
+	
+	this.init = function(){
+		var self = this;
+		window.onload = function(){
+			self.eventRender()
+		}
+	}
+	
+	VTemplate.prototype.eventRender = function(element){
+		element = element || null;
+		var self = this;
+		if(element !== null){
+			if(element.length === undefined)
+				element = [element];
+			var tempElements = element;
+		}else{
+			var tempElements = document.querySelectorAll('[data-vtemplate_' + self.tmpName + ']');
+		}
+		var tmpSplit = [];
+		var index = 'vtemplate_' + self.tmpName;
+		var target = '';
+		var targetVariable = '';
+		//console.log(tempElements);
+		for(var num = 0; num < tempElements.length; num++){
+			dataValue = tempElements[num].dataset[index];
+			tmpSplit = dataValue.split('=', 2);
+			target = tmpSplit[0];
+			targetVariable = tmpSplit[1];
+			switch(target){
+				case 'event'://event=click:order_click
+					var fSplit = targetVariable.split(':', 2);
+					if(typeof(self.eventFunctions[fSplit[1]]) != 'function'){
+						console.log('function: "' + fSplit[1] + '" not set in ' + tempElements[num].outerHTML);
+						continue;
+					}
+					self.addEvent(tempElements[num], fSplit[0], self.eventFunctions[fSplit[1]]);
+					break;
+			}
+		}
+		self.afterRender(tempElements);
+	}
 	
 	VTemplate.prototype.render = function(data, element){
 		element = element || null;
@@ -93,8 +153,9 @@ function VTemplate(params){
 					self.workElement = tempElements[num];
 					self.functions[fSplit[0]](fSplit[1]);
 					break;
-			}
 		}
 		self.afterRender(tempElements);
 	};
+	}
+	this.init();
 }
